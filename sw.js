2798dbdfc90ccc0cb42b43b8be4cde89377a452e
AmasiCarpets <hzkkp5s9jh@privaterelay.appleid.com>
2026-07-27 23:36:55 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE="billiards-duel-pro-v1";
+const ASSETS=["./","./index.html","./style.css","./script.js","./manifest.webmanifest"];
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
+self.addEventListener("fetch",event=>{
+  event.respondWith(
+    fetch(event.request).catch(()=>caches.match(event.request))
+  );
+});
